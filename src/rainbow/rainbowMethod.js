71b4e8376d99a50eaa431ff0a7ef0fc1b96eae74
elfarr <tonikaposik@gmail.com>
2024-02-01 22:36:12 +0300

    create rainbow

---
+++ src/rainbow/rainbowMethod.js
@@ -0,0 +1,19 @@
+import { hexToAnsiColor } from '../utils/color.js';
+
+export default function rainbow(text) {
+	let result = '';
+	const colors = [
+		'#FF0000',
+		'#FF7F00',
+		'#FFFF00',
+		'#00FF00',
+		'#00FFFF',
+		'#0000FF',
+		'#8F00FF',
+	];
+	for (let i = 0; i < text.length; i++) {
+		const color = hexToAnsiColor(colors[i % colors.length]);
+		result += color + text[i];
+	}
+	return result + '\x1b[0m';
+}
