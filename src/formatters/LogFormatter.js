233f67faade21ee654128b83ef9592986053a087
grefalb <grefalb11@gmail.com>
2024-02-07 22:09:00 +0300

    добавлен класс для настроек формата

--- src/formatters/LogFormatter.js
+++ src/formatters/LogFormatter.js
@@ -1,29 +1,97 @@
 import { hexToAnsiColor } from '../utils/color.js';
 
-export function printf(text, textFormat) {
+class PrivateSettings {
+	#color = '#ffffff';
+	#backgroundColor = 'transparent';
+	#style = 'normal';
+	#fontWeight = 'normal';
+	#textDecoration = 'none';
+
+	get color() {
+		return this.#color;
+	}
+	set color(hexColor) {
+		this.#color = hexColor;
+	}
+	get backgroundColor() {
+		return this.#backgroundColor;
+	}
+	set backgroundColor(hexBackgroundColor) {
+		this.#backgroundColor = hexBackgroundColor;
+	}
+	get style() {
+		return this.#style;
+	}
+	set style(style) {
+		this.#style = style;
+	}
+	get fontWeight() {
+		return this.#fontWeight;
+	}
+	set fontWeight(fontWeight) {
+		this.#fontWeight = fontWeight;
+	}
+	get textDecoration() {
+		return this.#textDecoration;
+	}
+	set textDecoration(textDecoration) {
+		this.#textDecoration = textDecoration;
+	}
+	getAll() {
+		return {
+			color: this.#color,
+			backgroundColor: this.#backgroundColor,
+			style: this.#style,
+			fontWeight: this.#fontWeight,
+			textDecoration: this.#textDecoration,
+		};
+	}
+	reset() {
+		this.#color = '#ffffff';
+		this.#backgroundColor = 'transparent';
+		this.#style = 'normal';
+		this.#fontWeight = 'normal';
+		this.#textDecoration = 'none';
+	}
+}
+
+const settings = new PrivateSettings();
+
+export default settings;
+
+export function printf(text) {
+	const formatMap = {
+		color: color => hexToAnsiColor(color),
+		backgroundColor: color =>
+			color == 'transparent'
+				? ''
+				: '\x1b[48' + hexToAnsiColor(color).slice(4),
+		style: {
+			normal: '\x1b[23m',
+			italic: '\x1b[3m',
+		},
+		fontWeight: {
+			normal: '\x1b[22m',
+			bold: '\x1b[1m',
+		},
+		textDecoration: {
+			none: '\x1b[24;29m',
+			underline: '\x1b[4m',
+			'line-through': '\x1b[9m',
+		},
+	};
+
 	let ansiFormatCode = '';
 
-	if (textFormat.color) ansiFormatCode += hexToAnsiColor(textFormat.color);
-
-	if (textFormat.backgroundColor)
-		ansiFormatCode +=
-			'\x1b[48' + hexToAnsiColor(textFormat.backgroundColor).slice(4);
-
-	if (textFormat.style)
-		if (textFormat.style == 'normal') ansiFormatCode += '\x1b[23m';
-		else if (textFormat.style == 'italic') ansiFormatCode += '\x1b[3m';
-
-	if (textFormat.fontWeight)
-		if (textFormat.fontWeight == 'normal') ansiFormatCode += '\x1b[22m';
-		else if (textFormat.fontWeight == 'bold') ansiFormatCode += '\x1b[1m';
-
-	if (textFormat.textDecoration)
-		if (textFormat.textDecoration == 'none')
-			ansiFormatCode += '\x1b[24;29m';
-		else if (textFormat.textDecoration == 'underline')
-			ansiFormatCode += '\x1b[4m';
-		else if (textFormat.textDecoration == 'line-through')
-			ansiFormatCode += '\x1b[9m';
-
+	for (const [key, value] of Object.entries(settings.getAll())) {
+		const format = formatMap[key];
+		if (format) {
+			if (typeof format === 'function') {
+				ansiFormatCode += format(value);
+			} else if (format[value]) {
+				ansiFormatCode += format[value];
+			}
+		}
+	}
 	console.log(ansiFormatCode + text + '\x1b[0m');
 }
