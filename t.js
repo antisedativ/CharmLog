61837f042f403ac690178cf288dffec2caf6400e
antisedative <antisedativ@mail.ru>
2024-02-10 22:31:19 +0300

    update dependabot

---
+++ t.js
@@ -0,0 +1,54 @@
+import charmlog from './src/index.js';
+import { printf, settings } from './src/index.js';
+
+import anyToString from './src/utils/anyToString.js';
+
+//console.log(anyToString(Symbol(1)));
+
+
+// settings.color = "#00ffdd"
+// charmlog.printf('test')
+// charmlog.printf(12345)
+// charmlog.printf(false)
+// charmlog.printf(Number(10))
+// charmlog.printf(10+10)
+// charmlog.printf([])
+// charmlog.printf({})
+// charmlog.printf([1,2,3,4])
+// charmlog.printf([1,'test', true, 4+10])
+// charmlog.printf({test: 10, test2: true, test3: {test4: [], test5: 'true'}})
+// charmlog.printf(Symbol(1))
+
+//printf(charmlog.settings.getAll());
+
+
+// console.log(settings.color);
+
+// settings.color = "#00ffdd"
+// printf('test 1 #00ffdd');
+
+// settings.reset();
+
+// printf('test 2 #ffffff');
+
+
+// backgroundColor = 'transparent';
+// style = 'normal';
+// fontWeight = 'normal';
+// textDecoration = 'none';
+
+// printf(settings.color);
+
+// printf('test 1');
+
+// settings.color = "#77ff";
+
+// printf('test 2');
+
+
+// import { hexToAnsiColor, print } from './src/utils/color.js';
+
+// print('test', "#77ff")
+// //print('test', "#77ff")
+
+console.log(Symbol(1))
