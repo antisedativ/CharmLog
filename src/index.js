d5da6c7b7e9b1be03b5e0abba966d59632d3bccf
antisedative <antisedativ@mail.ru>
2024-02-10 20:29:24 +0300

    fix main import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import rainbow from './rainbow/rainbowMethod.js';
 import { printf } from './formatters/LogFormatter.js';
 import settings from './formatters/LogFormatter.js';
 
-export const charmlog = { rainbow, printf, settings };
+const charmlog = { rainbow, printf, settings };
+
+export { rainbow, printf, settings };
 
 export default charmlog;
