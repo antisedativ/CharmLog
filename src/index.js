ceff6ec20b48111fc9589621da1adb8c05150eb0
grefalb <grefalb11@gmail.com>
2024-02-10 19:39:04 +0300

    добавил экспорт в index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import rainbow from './rainbow/rainbowMethod.js';
+import { printf } from './formatters/LogFormatter.js';
+import settings from './formatters/LogFormatter.js';
 
-export { rainbow };
+export { rainbow, printf, settings };
 
 export default {
 	rainbow,
+	printf,
+	settings,
 };
