09395030c7c4c0ccf028e0b43bc17fe0150e3e93
Nikita <vivat404@mail.ru>
2024-02-13 22:30:18 +0300

    Сделал иморт в index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,46 @@
 import rainbow from './rainbow/rainbowMethod.js';
 import printf from './formatters/LogFormatter.js';
 import settings from './formatters/settings.js';
+import {
+	orange,
+	yellow,
+	cyan,
+	white,
+	black,
+	magenta,
+	green,
+	red,
+	blue,
+} from './oneColor/colorMethod.js';
 
-const charmlog = { rainbow, printf, settings };
+const charmlog = {
+	rainbow,
+	printf,
+	settings,
+	orange,
+	yellow,
+	cyan,
+	white,
+	black,
+	magenta,
+	green,
+	red,
+	blue,
+};
 
-export { rainbow, printf, settings };
+export {
+	rainbow,
+	printf,
+	settings,
+	orange,
+	yellow,
+	cyan,
+	white,
+	black,
+	magenta,
+	green,
+	red,
+	blue,
+};
 
 export default charmlog;
--- src/oneColor/colorMethod.js
+++ src/oneColor/colorMethod.js
@@ -1,46 +1,46 @@
 import { hexToAnsiColor } from '../utils/color.js';
 
 export function red(value) {
-    const color = hexToAnsiColor('#FF0000');
-    return color + value + '\x1b[0m';
+	const color = hexToAnsiColor('#FF0000');
+	return color + value + '\x1b[0m';
 }
 
 export function orange(value) {
-    const color = hexToAnsiColor('#FF7F00');
-    return color + value + '\x1b[0m';
+	const color = hexToAnsiColor('#FF7F00');
+	return color + value + '\x1b[0m';
 }
 
 export function yellow(value) {
-    const color = hexToAnsiColor('#FFFF00') ;
-    return color + value + '\x1b[0m';
+	const color = hexToAnsiColor('#FFFF00');
+	return color + value + '\x1b[0m';
 }
 
 export function green(value) {
-    const color = hexToAnsiColor('#00FF00');
-    return color + value + '\x1b[0m';
+	const color = hexToAnsiColor('#00FF00');
+	return color + value + '\x1b[0m';
 }
 
 export function blue(value) {
-    const color = hexToAnsiColor('#0000FF');
-    return color + value + '\x1b[0m';
+	const color = hexToAnsiColor('#0000FF');
+	return color + value + '\x1b[0m';
 }
 
 export function magenta(value) {
-    const color = hexToAnsiColor('#bb2649');
-    return color + value + '\x1b[0m';
+	const color = hexToAnsiColor('#bb2649');
+	return color + value + '\x1b[0m';
 }
 
 export function cyan(value) {
-    const color = hexToAnsiColor('#00FFFF');
-    return color + value + '\x1b[0m';
+	const color = hexToAnsiColor('#00FFFF');
+	return color + value + '\x1b[0m';
 }
 
 export function white(value) {
-    const color = hexToAnsiColor('#FFFFFF');
-    return color + value + '\x1b[0m';
+	const color = hexToAnsiColor('#FFFFFF');
+	return color + value + '\x1b[0m';
 }
 
 export function black(value) {
-    const color = hexToAnsiColor('#000000');
-    return color + value + '\x1b[0m';
+	const color = hexToAnsiColor('#000000');
+	return color + value + '\x1b[0m';
 }
