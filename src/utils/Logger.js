649179b7fd129c319b5177ec46fedaad3ea16915
elfarr <tonikaposik@gmail.com>
2024-02-15 19:15:46 +0300

    logger sigle mode

--- src/utils/Logger.js
+++ src/utils/Logger.js
@@ -1,36 +1,49 @@
 import { hexToAnsiColor } from '../utils/color.js';
 const logColor = ['#FFFFFF', '#0000FF', '#FF8000', '#F14065', '#FF0000'];
 class Logger {
-    constructor(logLevel) {
-      this.logLevel = logLevel || 'info';
+  constructor(logLevel, single_mode) {
+    this.logLevel = logLevel || 'debug';
+    this.single_mode = single_mode || false;
+  }
+  setLogLevel(logLevel, single_mode = false) {
+    this.logLevel = logLevel;
+    this.single_mode = single_mode;
+  }
+  log(message, level = 'info') {
+    const levels = ['debug', 'info', 'warning', 'error', 'critical'];
+    const levelIndex = levels.indexOf(level);
+    const currentLevelIndex = levels.indexOf(this.logLevel);
+    if (this.single_mode == true) {
+      if (levelIndex == currentLevelIndex) {
+        console.log(`${hexToAnsiColor(logColor[levelIndex])}[${level.toUpperCase()}] ${message}\x1b[0m`);
+      }
     }
-    setLogLevel(logLevel) {
-      this.logLevel = logLevel;
-    }
-    log(message, level = 'info') {
-      const levels = ['debug', 'info', 'warning', 'error', 'critical'];
-      const levelIndex = levels.indexOf(level);
-      const currentLevelIndex = levels.indexOf(this.logLevel);
+    else {
       if (levelIndex >= currentLevelIndex) {
         console.log(`${hexToAnsiColor(logColor[levelIndex])}[${level.toUpperCase()}] ${message}\x1b[0m`);
       }
     }
-    debug(message) {
-      this.log(message, 'debug');
-    }
-    info(message) {
-      this.log(message, 'info');
-    }
-  
-    warning(message) {
-      this.log(message, 'warning');
-    }
-  
-    error(message) {
-      this.log(message, 'error');
-    }
-  
-    critical(message) {
-      this.log(message, 'critical');
-    }
   }
+  reset() {
+    this.single_mode = false;
+    this.logLevel = 'debug';
+  }
+  debug(message) {
+    this.log(message, 'debug');
+  }
+  info(message) {
+    this.log(message, 'info');
+  }
+
+  warning(message) {
+    this.log(message, 'warning');
+  }
+
+  error(message) {
+    this.log(message, 'error');
+  }
+
+  critical(message) {
+    this.log(message, 'critical');
+  }
+}
