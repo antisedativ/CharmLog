cf7a4389dff2bc346856d49c28d7a863ee3392ed
antisedative <antisedativ@mail.ru>
2024-02-18 18:11:03 +0300

    lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,9 @@
 export default {
-    input: 'src/index.js',
-    output: {
-      file: 'dist/bundle.js',
-      format: 'umd',
-      name: 'charmlog',
-      sourcemap: true,
-    },
-  };
-  
+	input: 'src/index.js',
+	output: {
+		file: 'dist/bundle.js',
+		format: 'umd',
+		name: 'charmlog',
+		sourcemap: true,
+	},
+};
