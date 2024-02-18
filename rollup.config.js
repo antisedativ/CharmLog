8189c16be3405108a47345aa24152d8157f02b54
antisedative <antisedativ@mail.ru>
2024-02-18 18:29:22 +0300

    added support for ESM and CommonJS versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,20 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
 export default {
-	input: 'src/index.js',
-	output: {
-		file: 'dist/bundle.js',
-		format: 'umd',
-		name: 'charmlog',
-		sourcemap: true,
-	},
+	input: 'src/index.js', // путь к вашему исходному файлу
+	output: [
+		{
+			file: 'dist/index.cjs.js', // CommonJS-совместимая версия
+			format: 'cjs',
+		},
+		{
+			file: 'dist/index.mjs', // ESM-совместимая версия
+			format: 'esm',
+		},
+	],
+	plugins: [
+		resolve(), // разрешает импорты из node_modules
+		commonjs(), // преобразует модули CommonJS в ES6
+	],
 };
