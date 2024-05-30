618edf56f77b0b501b4e39d7d807b221a005f752
antisedative <antisedativ@mail.ru>
2024-05-31 01:57:40 +0300

    eslint updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,18 +15,25 @@ module.exports = {
 				sourceType: 'script',
 			},
 		},
+		{
+			files: ['src/**/*.{js,jsx,ts,tsx}'], // Проверять только файлы в папке src
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+			},
+			rules: {
+				'vue/multi-word-component-names': 0,
+				semi: ['error', 'always', { omitLastInOneLineBlock: true }],
+				quotes: ['error', 'single', { allowTemplateLiterals: true }],
+				'no-trailing-spaces': ['error'],
+				'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1 }],
+				'eol-last': ['error', 'always'],
+			},
+		},
 	],
 	plugins: ['es6'],
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
-	},
-	rules: {
-		'vue/multi-word-component-names': 0,
-		semi: ['error', 'always', { omitLastInOneLineBlock: true }],
-		quotes: ['error', 'single', { allowTemplateLiterals: true }],
-		'no-trailing-spaces': ['error'],
-		'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1 }],
-		'eol-last': ['error', 'always'],
 	},
 };
