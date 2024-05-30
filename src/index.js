c1fb6bdabfeb27ba648b64d367d3e97a993f2308
antisedative <antisedativ@mail.ru>
2024-05-31 01:55:19 +0300

    conflict resolved

--- src/index.js
+++ src/index.js
@@ -3,34 +3,19 @@ import printf from './formatters/LogFormatter.js';
 import settings from './formatters/settings.js';
 import { colors } from './oneColor/colorMethod.js';
 import { Logger } from './logger/Logger.js';
-<<<<<<< CLT-33
 import { createCharmedObject, charmprint } from './charmprint.js';
-=======
 import dir from './dir/dir.js';
->>>>>>> master
 
 const charmlogMethods = {
 	rainbow,
 	printf,
 	settings,
 	...colors,
-<<<<<<< CLT-33
 	charmprint,
-=======
-	dir
+	dir,
 };
 
-export {
-	rainbow,
-	printf,
-	settings,
->>>>>>> master
-	Logger,
-	dir
-};
-
-export { rainbow, printf, settings, Logger, charmprint };
 const charmlog = createCharmedObject(charmlogMethods);
 
+export { rainbow, printf, settings, Logger, dir, charmprint };
 export default charmlog;
-
