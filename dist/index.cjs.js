9fbc0f2c0310840eae8bcecf2062c618e3378c07
antisedative <antisedativ@mail.ru>
2024-08-27 22:20:13 +0300

    new build

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -25,6 +25,22 @@ const logLevelColors = {
     critical: color.red
 };
 
+const TEXT_DECORATIONS = {
+    NONE: 'none',
+    UNDERLINE: 'underline',
+    LINE_THROUGH: 'line-through',
+};
+
+const FONT_WEIGHTS = {
+    NORMAL: 'normal',
+    BOLD: 'bold',
+};
+
+const STYLES = {
+    NORMAL: 'normal',
+    ITALIC: 'italic',
+};
+
 function hexToAnsiColor(hexColor) {
 	if (!hexRegex.test(hexColor)) {
 		console.error('Invalid HEX color format');
@@ -44,7 +60,6 @@ function hexToAnsiColor(hexColor) {
 	return `\x1b[38;2;${red};${green};${blue}m`;
 }
 
-// Convert any value to string
 function anyToString(value) {
 	if (typeof value === 'string') return value;
 	try {
@@ -64,120 +79,165 @@ const colors$1 = [
 	'#8F00FF',
 ];
 
-function rainbow(value) {
-	let text = anyToString(value);
+function rainbow(...values) {
 	let result = '';
-	for (let i = 0; i < text.length; i++) {
-		const color = hexToAnsiColor(colors$1[i % colors$1.length]);
-		result += color + text[i];
-	}
-	console.log(result + '\x1b[0m');
+	values.forEach((value, index) => {
+		let text = anyToString(value);
+		for (let i = 0; i < text.length; i++) {
+			const color = hexToAnsiColor(colors$1[(i + index) % colors$1.length]);
+			result += color + text[i];
+		}
+	});
+	return result + '\x1b[0m';
 }
 
 function colorValid(color) {
-	if (hexRegex.test(color)) return 1;
+	if (hexRegex.test(color)) return true;
 	else {
 		console.error('Invalid color format');
-		return 0;
+		return false;
 	}
 }
 
 function backgroundColorValid(backgroundColor) {
+
 	if (hexRegex.test(backgroundColor) || backgroundColor == 'transparent')
-		return 1;
+		return true;
 	else {
 		console.error('Invalid background color format');
-		return 0;
+		return false;
 	}
 }
 
 function styleValid(style) {
-	const styleOptions = ['normal', 'italic'];
-	if (styleOptions.includes(style)) return 1;
+	const styleOptions = Object.values(STYLES);
+	if (styleOptions.includes(style)) return true;
 	else {
 		console.error('Invalid style format');
-		return 0;
+		return false;
 	}
 }
 
 function fontWeightValid(fontWeight) {
-	const fontWeightOptions = ['normal', 'bold'];
-	if (fontWeightOptions.includes(fontWeight)) return 1;
+	const fontWeightOptions = Object.values(FONT_WEIGHTS);
+	if (fontWeightOptions.includes(fontWeight)) return true;
 	else {
 		console.error('Invalid font weight format');
-		return 0;
+		return false;
 	}
 }
 
 function textDecorationValid(textDecoration) {
-	const textDecorationOptions = ['none', 'underline', 'line-through'];
-	if (textDecorationOptions.includes(textDecoration)) return 1;
+	const textDecorationOptions = Object.values(TEXT_DECORATIONS);
+	if (textDecorationOptions.includes(textDecoration)) return true;
 	else {
 		console.error('Invalid text decoration format');
-		return 0;
+		return false;
 	}
 }
 
 class PrivateSettings {
-	#color = '#ffffff';
-	#backgroundColor = 'transparent';
-	#style = 'normal';
-	#fontWeight = 'normal';
-	#textDecoration = 'none';
 
-	get color() {
-		return this.#color;
-	}
-	set color(hexColor) {
-		if (colorValid(hexColor)) this.#color = hexColor;
-	}
-	get backgroundColor() {
-		return this.#backgroundColor;
-	}
-	set backgroundColor(backgroundColor) {
-		if (backgroundColorValid(backgroundColor))
-			this.#backgroundColor = backgroundColor;
-	}
-	get style() {
-		return this.#style;
-	}
-	set style(style) {
-		if (styleValid(style)) this.#style = style;
-	}
-	get fontWeight() {
-		return this.#fontWeight;
-	}
-	set fontWeight(fontWeight) {
-		if (fontWeightValid(fontWeight)) this.#fontWeight = fontWeight;
-	}
-	get textDecoration() {
-		return this.#textDecoration;
-	}
-	set textDecoration(textDecoration) {
-		if (textDecorationValid(textDecoration))
-			this.#textDecoration = textDecoration;
-	}
-	getAll() {
-		return {
-			color: this.#color,
-			backgroundColor: this.#backgroundColor,
-			style: this.#style,
-			fontWeight: this.#fontWeight,
-			textDecoration: this.#textDecoration,
-		};
-	}
-	reset() {
-		this.#color = '#ffffff';
-		this.#backgroundColor = 'transparent';
-		this.#style = 'normal';
-		this.#fontWeight = 'normal';
-		this.#textDecoration = 'none';
-	}
+    #color = '#ffffff';
+    #backgroundColor = 'transparent';
+    #style = STYLES.NORMAL;
+    #fontWeight = FONT_WEIGHTS.NORMAL;
+    #textDecoration = TEXT_DECORATIONS.NONE;
+
+    get color() {
+        return this.#color;
+    }
+	/**
+ * Цвет текста в формате HEX (например, '#ffffff')
+ * @type {string}
+ */
+    set color(hexColor) {
+        if (colorValid(hexColor)) this.#color = hexColor;
+    }
+/**
+ * Цвет текста в формате HEX (например, '#ffffff') или transparent
+ * @type {string}
+ */
+    get backgroundColor() {
+        return this.#backgroundColor;
+    }
+    set backgroundColor(backgroundColor) {
+        if (backgroundColorValid(backgroundColor))
+            this.#backgroundColor = backgroundColor;
+    }
+
+    get style() {
+        return this.#style;
+    }
+    set style(style) {
+        if (styleValid(style)) this.#style = style;
+    }
+
+    get fontWeight() {
+        return this.#fontWeight;
+    }
+    set fontWeight(fontWeight) {
+        if (fontWeightValid(fontWeight)) this.#fontWeight = fontWeight;
+    }
+
+    get textDecoration() {
+        return this.#textDecoration;
+    }
+    set textDecoration(textDecoration) {
+        if (textDecorationValid(textDecoration))
+            this.#textDecoration = textDecoration;
+    }
+
+    setTextDecorationNone() {
+        this.textDecoration = TEXT_DECORATIONS.NONE;
+    }
+
+    setTextDecorationUnderline() {
+        this.textDecoration = TEXT_DECORATIONS.UNDERLINE;
+    }
+
+    setTextDecorationLineThrough() {
+        this.textDecoration = TEXT_DECORATIONS.LINE_THROUGH;
+    }
+
+    setFontWeightNormal() {
+        this.fontWeight = FONT_WEIGHTS.NORMAL;
+    }
+
+    setFontWeightBold() {
+        this.fontWeight = FONT_WEIGHTS.BOLD;
+    }
+
+    setStyleNormal() {
+        this.style = STYLES.NORMAL;
+    }
+
+    setStyleItalic() {
+        this.style = STYLES.ITALIC;
+    }
+
+    getAll() {
+        return {
+            color: this.#color,
+            backgroundColor: this.#backgroundColor,
+            style: this.#style,
+            fontWeight: this.#fontWeight,
+            textDecoration: this.#textDecoration,
+        };
+    }
+
+    reset() {
+        this.#color = '#ffffff';
+        this.#backgroundColor = 'transparent';
+        this.#style = STYLES.NORMAL;
+        this.#fontWeight = FONT_WEIGHTS.NORMAL;
+        this.#textDecoration = TEXT_DECORATIONS.NONE;
+    }
 }
 
 const settings = new PrivateSettings();
 
-function printf(text) {
+function printf(...args) {
 	const formatMap = {
 		color: color => hexToAnsiColor(color),
 		backgroundColor: color =>
@@ -211,21 +271,21 @@ function printf(text) {
 			}
 		}
 	}
-	console.log(ansiFormatCode + anyToString(text) + '\x1b[0m');
+	args.map(anyToString).join(' ');
 }
 
-const handler = text => anyToString(text) + '\x1b[0m';
+const handler = text => anyToString(text);
 
 const colors = {
-	red: value => console.log(color.red + handler(value)),
-	orange: value => console.log(color.orange + handler(value)),
-	yellow: value => console.log(color.yellow + handler(value)),
-	green: value => console.log(color.green + handler(value)),
-	blue: value => console.log(color.blue + handler(value)),
-	magenta: value => console.log(color.magenta + handler(value)),
-	cyan: value => console.log(color.cyan + handler(value)),
-	white: value => console.log(color.white + handler(value)),
-	black: value => console.log(color.black + handler(value)),
+	red: (...values) => color.red + values.map(handler).join(' ') + '\x1b[0m',
+	orange: (...values) => color.orange + values.map(handler).join(' ') + '\x1b[0m',
+	yellow: (...values) => color.yellow + values.map(handler).join(' ') + '\x1b[0m',
+	green: (...values) => color.green + values.map(handler).join(' ') + '\x1b[0m',
+	blue: (...values) => color.blue + values.map(handler).join(' ') + '\x1b[0m',
+	magenta: (...values) => color.magenta + values.map(handler).join(' ') + '\x1b[0m',
+	cyan: (...values) => color.cyan + values.map(handler).join(' ') + '\x1b[0m',
+	white: (...values) => color.white + values.map(handler).join(' ') + '\x1b[0m',
+	black: (...values) => color.black + values.map(handler).join(' ') + '\x1b[0m',
 };
 
 class Logger {
@@ -239,7 +299,7 @@ class Logger {
     this.single_mode = single_mode;
   }
 
-  log(message, level = 'info') {
+  log(message, level = 'debug') {
     const levels = ['debug', 'info', 'warning', 'error', 'critical'];
     const levelIndex = levels.indexOf(level);
     const currentLevelIndex = levels.indexOf(this.logLevel);
@@ -273,15 +333,64 @@ class Logger {
   }
 }
 
-const charmlog = {
+function charmprint(func) {
+	return function (...args) {
+		const result = func.apply(this, args);
+		if (typeof result === 'string') {
+			console.log(`${result}`);
+		}
+		return result;
+	};
+}
+
+function createCharmedObject(obj) {
+	return new Proxy(obj, {
+		get(target, property, receiver) {
+			const origMethod = target[property];
+			if (typeof origMethod === 'function') {
+				return charmprint(origMethod).bind(target);
+			}
+			return Reflect.get(target, property, receiver);
+		},
+	});
+}
+
+function dir(obj, depth = 0, indent = '  ') {
+    const indentation = indent.repeat(depth);
+    let result = '';
+    for (let prop in obj) {
+        if (typeof obj[prop] === 'object' && obj[prop] !== null) {
+            if (obj[prop] instanceof Set || obj[prop] instanceof Array) {
+                const type = Object.prototype.toString.call(obj[prop]).slice(8, -1);
+                result += `${indentation}${colors.red(prop)}: ${colors.blue(type, [...obj[prop]])}\n`;
+            } else {
+                result += `${indentation}${colors.red(prop)}:\n`;
+                result += dir(obj[prop], depth + 1, indent);
+            }
+        } else if (typeof obj[prop] === 'function') {
+            result += `${indentation}${colors.red(prop)}: ${colors.blue('function')}\n`;
+        } else {
+            result += `${indentation}${colors.red(prop)}: ${colors.blue(obj[prop])}\n`;
+        }
+    }
+    return result;
+}
+
+const charmlogMethods = {
 	rainbow,
 	printf,
 	settings,
-	...colors
+	...colors,
+	charmprint,
+	dir,
 };
 
+const charmlog = createCharmedObject(charmlogMethods);
+
 exports.Logger = Logger;
+exports.charmprint = charmprint;
 exports.default = charmlog;
+exports.dir = dir;
 exports.printf = printf;
 exports.rainbow = rainbow;
 exports.settings = settings;
